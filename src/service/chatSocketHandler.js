52665f6a3600ff631cb7327e86a1e9de57aa5f95
goziri71 <goziri71@gmail.com>
2025-07-24 15:11:46 +0100

    updating

--- src/service/chatSocketHandler.js
+++ src/service/chatSocketHandler.js
@@ -251,6 +251,7 @@ const sendNotificationToRecipients = async (
         const payload = {
           title: senderName,
           body: message.content,
+          unreadCount: unreadCount,
           icon: "/images/redbiller.png",
           badge: "/images/redbiller.png",
           currentUserId: recipientId,
